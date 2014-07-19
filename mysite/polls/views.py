bcf8217ba80f3df74213665d62a1d26ea12cfbbd
Jim Lynch <jim@jam.sessionsnet.org>
2014-07-18 20:30:06 -0700

    make polls use django.shortchts render

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,15 +1,16 @@
+# used by detail, results and vote
 from django.http import HttpResponse
+# no longer used -- it's all in the shortcut render
 from django.template import RequestContext, loader
+# used by index only atm
+from django.shortcuts import render
 
 from polls.models import Poll
 
 def index(request):
     latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
-    context = RequestContext(request, {
-        'latest_poll_list': latest_poll_list,
-    })
-    return HttpResponse(template.render(context))
+    context = {'latest_poll_list': latest_poll_list,}
+    return render(request, 'polls/index.html', context)
 
 def detail(request, poll_id):
     return HttpResponse("You're looking at poll %s." % poll_id)
